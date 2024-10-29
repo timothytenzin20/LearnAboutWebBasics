8b49bf5389a61f005387af6b2686cf00976828fa
timothytenzin20 <timothytenzin2004@gmail.com>
2024-10-29 02:53:58 -0400

    Workshop preparation

--- JavascriptIntroduction/projects.js
+++ JavascriptIntroduction/projects.js
@@ -1,4 +1,4 @@
-// Step 4. Represent all this in an Objects
+// 4. Represent all this in an Objects
 let person = {
     name: "John",
     age: 25,
@@ -6,80 +6,26 @@ let person = {
     colors: ["red", "green", "blue"]
 }
 
-// Step 1. Variables
-// Variables are containers for storing data values.
-w = 7; // Automatically Set
-var x = 8; // Can be changed, but for OLD BROWSERS
-let y = 9; // Can be changed, but for NEW BROWSERS
-const z = 10; // Cannot be changed
-document.getElementById("demo").innerHTML = w;
-
+// 1a. Variables are containers for storing data values.
 // Try to use const, then let, then var
 
-// Step 2. Data Types
+// 1b Experiment with the different variable notations. 
+
+// 2. Data Types
 // Strings
-let name = "John";
 // Numbers
-let age = 25;
 // Booleans
-let isAdult = true;
 // Arrays
-let colors = ["red", "green", "blue"];
 
-document.getElementById("info").innerHTML = "Hi my name is " + name + ", I am " + age + " years old.";
+// 2a. Using multiple variables
 
+// 3. Change the color of the info tag
+// 3a. setInterval is a function that runs a function every x milliseconds
+// 3b. Use Arrow Functions
 
-// Step 3. Change the color of the info tag
-let i = 0;
-// setInterval is a function that runs a function every x milliseconds
-setInterval(() => {
-    if (i == person.colors.length) {
-            i = 0;
-        }
-        document.getElementById("info").style.color = person.colors[i];
-        i++;
-    }, 1000);
+// 5. Display date when we click button using event handlers
 
 
-// Move this up to the top and change the code to use the object
+// 6. List your projects in an object
 
-// Switch to slideshow and explain controls
-
-// Step 5. Display date when we click button using event handlers
-function displayDate() {
-    document.getElementById("date").innerHTML = Date();
-}
-
-// Step 6. List your projects in an object
-let projects = {
-    project1: {
-        name: "Project 1",
-        description: "This is project 1",
-        link: "https://www.google.com",
-        transition: "slide"
-    },
-    project2: {
-        name: "Project 2",
-        description: "This is project 2",
-        link: "https://www.google.com",
-        transition: "fade"
-    },
-    project3: {
-        name: "Project 3",
-        description: "This is project 3",
-        link: "https://www.google.com",
-        transition: "slide"
-    }
-}
-
-// Step 6a. Display projects when we hover over a title
-let projectsDisplayed = false;
-
-function displayProjects() {
-    if (!projectsDisplayed) {
-        for (let project in projects) {
-            document.getElementById("projects").innerHTML += "<a href='" + projects[project].link + "'>" + projects[project].name + "</a><br>";
-        }
-        projectsDisplayed = true;
-    }
-}
+// 6a. Display the projects in a list
