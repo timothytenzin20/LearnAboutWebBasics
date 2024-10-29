73e86c1060c53b7dbacd39e2999fec9a44b7255c
timothytenzin20 <timothytenzin2004@gmail.com>
2024-10-29 03:58:04 -0400

    Workshop completion

--- JavascriptIntroduction/projects.js
+++ JavascriptIntroduction/projects.js
@@ -1,10 +1,4 @@
 // 4. Represent all this in an Objects
-let person = {
-    name: "John",
-    age: 25,
-    isAdult: true,
-    colors: ["red", "green", "blue"]
-}
 
 // 1a. Variables are containers for storing data values.
 // Try to use const, then let, then var
@@ -18,13 +12,11 @@ let person = {
 // Arrays
 
 // 2a. Using multiple variables
-
 // 3. Change the color of the info tag
 // 3a. setInterval is a function that runs a function every x milliseconds
 // 3b. Use Arrow Functions
 
 // 5. Display date when we click button using event handlers
-
 
 // 6. List your projects in an object
 
